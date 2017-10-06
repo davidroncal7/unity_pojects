f18e1629ad5fb40f5e702de2459b22c78de06afd
davidroncal <david_roncal@hotmail.com>
2017-10-06 19:43:24 +0200

    Setting Camera and Moving Camera

---
+++ roll_a_ball/Assets/scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject main_ball;
+
+	private Vector3 v3_offset_position;
+
+	// Use this for initialization
+	void Start () 
+	{
+		v3_offset_position = transform.position - main_ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = main_ball.transform.position + v3_offset_position;
+	}
+}
